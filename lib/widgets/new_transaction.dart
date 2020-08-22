d2cf0892757e6af50062604cdf7335a45daefdd9
Mukesh Soni <msony552@gmail.com>
2020-08-22 16:18:01 +0530

    keyboard padding adjustments for bottom sheet

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -54,7 +54,12 @@ class _NewTransactionState extends State<NewTransaction> {
     return Card(
       elevation: 5,
       child: Container(
-        padding: EdgeInsets.all(10.0),
+        padding: EdgeInsets.only(
+          top: 10,
+          left: 10,
+          right: 10,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
