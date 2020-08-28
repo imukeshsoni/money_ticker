65354088eba5831e283d6ac9f457c814da64fce0
Mukesh Soni <msony552@gmail.com>
2020-08-28 20:44:31 +0530

    delete button modified according to size of device

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -51,86 +51,88 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.only(
-          top: 10,
-          left: 10,
-          right: 10,
-          bottom: MediaQuery.of(context).viewInsets.bottom + 10,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.title),
-                contentPadding: EdgeInsets.all(20.0),
-                labelText: 'Title',
-                errorMaxLines: 1,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-              ),
-              controller: _titleController,
-              onSubmitted: (_) => _submitData(),
-              // onChanged: (value) {
-              //   titleInput = value;
-              // },
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.attach_money),
-                labelText: 'Amount',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-              ),
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitData(),
-              // onChanged: (value) {
-              //   amountInput = value;
-              // },
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      _selectedDate == null
-                          ? 'No Date Chosen'
-                          : "Picked Date : ${DateFormat.yMMMd().format(_selectedDate)}",
-                    ),
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.title),
+                  contentPadding: EdgeInsets.all(20.0),
+                  labelText: 'Title',
+                  errorMaxLines: 1,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
-                  FlatButton(
-                    child: Text(
-                      'Choose Date',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColor,
+                ),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+                // onChanged: (value) {
+                //   titleInput = value;
+                // },
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.attach_money),
+                  labelText: 'Amount',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData(),
+                // onChanged: (value) {
+                //   amountInput = value;
+                // },
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? 'No Date Chosen'
+                            : "Picked Date : ${DateFormat.yMMMd().format(_selectedDate)}",
                       ),
                     ),
-                    onPressed: _presentDatePicker,
-                  )
-                ],
-              ),
-            ),
-            FlatButton(
-              child: Text(
-                'Add transaction',
-                style: TextStyle(
-                  color: Colors.white,
+                    FlatButton(
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      onPressed: _presentDatePicker,
+                    )
+                  ],
                 ),
               ),
-              color: Theme.of(context).primaryColor,
-              onPressed: _submitData,
-            ),
-          ],
+              FlatButton(
+                child: Text(
+                  'Add transaction',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                color: Theme.of(context).primaryColor,
+                onPressed: _submitData,
+              ),
+            ],
+          ),
         ),
       ),
     );
